337fb8fcde1b25b984452b1e8c8776db6db0e4ab
vinicivs-rocha <viniciusroch65@gmail.com>
2024-04-10 18:11:09 -0300

    feat: add query params

--- handlers/viewSnippet.go
+++ handlers/viewSnippet.go
@@ -1,7 +1,16 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func ViewSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display some snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id <= 0 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Displaying snippet with id %d", id)
 }
