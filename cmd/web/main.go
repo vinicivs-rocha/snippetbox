26949e33c9ca5e96c9ae96c18e172b43e2263efa
vinicivs-rocha <viniciusroch65@gmail.com>
2024-04-12 07:40:32 -0300

    feat: add static files serving

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", viewSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Listening on :8080")
 	err := http.ListenAndServe(":8080", mux)
